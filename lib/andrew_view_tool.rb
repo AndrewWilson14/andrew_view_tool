ac1e432c55daeddd978161d690b91c2f094ddda1
Andrew Wilson <andrewwilson1414@gmail.com>
2017-03-08 00:48:12 -0700

    Dev tool initial commit

---
+++ lib/andrew_view_tool.rb
@@ -0,0 +1,6 @@
+require "andrew_view_tool/version"
+require "andrew_view_tool/renderer"
+
+module AndrewViewTool
+  # Your code goes here...
+end
---
+++ lib/andrew_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AndrewViewTool
+  VERSION = "0.1.0"
+end
